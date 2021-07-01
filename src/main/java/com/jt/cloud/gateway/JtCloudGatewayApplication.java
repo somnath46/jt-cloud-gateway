d31a87dc0a457eb94ae0d38f5ce4951c322c9d84
somnath46 <4684.somnath@gmail.com>
2021-07-01 20:28:15 +0530

    enable hystrix for hystrix dashboard #3

--- src/main/java/com/jt/cloud/gateway/JtCloudGatewayApplication.java
+++ src/main/java/com/jt/cloud/gateway/JtCloudGatewayApplication.java
@@ -3,9 +3,11 @@ package com.jt.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class JtCloudGatewayApplication {
 
 	public static void main(String[] args) {
